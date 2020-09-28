06ccd3bcfb0f10e3d8ace483aa91c6e716141d9e
tdunker <tdunker@nanocosmos.de>
2020-09-28 17:31:47 +0200

    add vuejs-sample code project

---
+++ nanoplayer-vuejs-sample/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <NanoPlayer/>
+  </div>
+</template>
+
+<script>
+import NanoPlayer from './components/NanoPlayer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NanoPlayer
+  }
+}
+</script>
